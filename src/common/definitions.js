299a9957aec6ad7f81c3bb9fb4b2cc5e4248382e
Guilherme Lima Goncalves <guligo@tutanota.com>
2023-06-23 16:12:23 -0400

    docs: make auxiliary functions' purpose more evident

--- src/common/definitions.js
+++ src/common/definitions.js
@@ -1,22 +1,28 @@
 const validReferOrigin = 'http://sso.company.com:3010';
 const ssoBaseApiEndpint = `${validReferOrigin}/sso`;
+
+// Endpoint to verify the client ID and generate a new JWT
 const ssoServerJwtUrl = (ssoToken) =>
   `${ssoBaseApiEndpint}/verifytoken?ssoToken=${ssoToken}`;
+
+// Endpoint for redirecting the client app to the SSO service
 const ssoServerLoginUrl = (serviceUrl) =>
   `${ssoBaseApiEndpint}/login?serviceURL=${serviceUrl}`;
 
-// app token to validate the request is coming from the authenticated server only.
+// App token to validate the request is coming from the authenticated server only.
 const appTokenDB = {
   sso_consumer: 'l1Q7zkOL59cRqWBkQ12ZiGVW2DBL',
   simple_sso_consumer: '1g0jJwGmRQhJwvwNOrY4i90kD0m'
 };
 
+// Map that determines if the corresponding service can access the SSO
 const allowedOrigin = {
   'http://consumer.one.company.com:3020': true,
   'http://consumer.two.company.com:3030': true,
   'http://sso.company.com:3080': false
 };
 
+// Perform the mapping between the service name and origin URL
 const originAppName = {
   'http://consumer.one.company.com:3020': 'sso_consumer',
   'http://consumer.two.company.com:3030': 'simple_sso_consumer'
